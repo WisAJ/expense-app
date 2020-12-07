7a9db3e5fe946a0f186be4a054d279ab1e79974b
WisAJ <wissamgh@hotmail.com>
2020-12-07 11:53:02 +0100

    Add expenses summary to the Dashboard

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,3 +1,8 @@
 export default (expenses) => {
         return expenses.reduce((sum, value) => sum + value.amount, 0); 
 };
+
+//  another way with map:
+// return expenses
+//         .map((expense)=> expense.amount)
+//         .reduce((sum, value) => sum + value, 0);
