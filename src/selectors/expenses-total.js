de71f14eb58359cee97e62a1625572a35e9e6e5e
WisAJ <wissamgh@hotmail.com>
2020-12-20 19:38:45 +0100

    test added

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,8 +1,5 @@
 export default (expenses) => {
-        return expenses.reduce((sum, value) => sum + value.amount, 0); 
+  return expenses
+      .map((expense) => expense.amount)
+      .reduce((sum, value) => sum + value, 0);
 };
-
-//  another way with map:
-// return expenses
-//         .map((expense)=> expense.amount)
-//         .reduce((sum, value) => sum + value, 0);
