7b5aa6ff748123552e1c040b912e69d7efc0db3e
WisAJ <wissamgh@hotmail.com>
2020-12-21 01:20:56 +0100

    firebase fixed issue

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,14 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyDscZhQhtGV_vsU-fkHGewvqPhOtLgiFXg",
+  authDomain: "expensifyapp-e2720.firebaseapp.com",
+  databaseURL: "https://expensifyapp-e2720-default-rtdb.firebaseio.com",
+  projectId: "expensifyapp-e2720",
+  storageBucket: "expensifyapp-e2720.appspot.com",
+  messagingSenderId: "948886015328",
+  appId: "1:948886015328:web:9d5f210e91246d20bce205",
+  measurementId: "G-7816PX8NYR"
 };
 
 firebase.initializeApp(config);
